5cdb0a5109ddea411d0fd5b70e970c046c05c6c9
vikasmirdoodi93 <mvikasmudiraj@gmail.com>
2020-02-04 23:09:22 -0600

    added server logic for passwords

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE)
+mongoose.connect('mongodb://localhost:27017/waves')
 .then(connection => {
     console.log('Connected to MongoDB')
 })
@@ -20,14 +20,35 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get('/', function(req,res, next){
-    res.send("middle");
-    next();
-    console.log('one')
+//models
+const { User } = require('./models/user')
+
+// users
+app.post('/api/users/register',(req,res)=>{
+    const user = new User(req.body);
+    user.save((err, doc)=>{
+        if(err) return res.json({success:false,err});
+        res.status(200).json({
+            success: true,
+            userdata: doc
+        })
+    })
 })
 
-
-app.post()
+app.post('/api/users/login', (req,res)=>{
+ User.findOne({'email': req.body.email}, (err, user)=>{
+        if(!user) return res.json({loginSucess: false, message:'Auth Failed, no user found'});
+        user.comparePassword(req.body.password,(err,isMatch)=>{
+         if(!isMatch) return res.json({loginSucess: false,message:'Wrong Password'});
+         user.generateToken((err,user)=>{
+         if(err) return res.status(400).send(err);
+         res.cookie('w_auth',user.token).status(200).json({
+             loginSucess:true
+         })
+         })
+        })
+    })
+})
 
 const port = process.env.PORT || 3002;
 
