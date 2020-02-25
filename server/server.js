ed72e67d0280707a0debb8e658d7e7fad15076b6
vikasmirdoodi93 <mvikasmudiraj@gmail.com>
2020-02-24 18:17:41 -0600

    done basicserver side setup

--- server/server.js
+++ server/server.js
@@ -23,11 +23,102 @@ app.use(cookieParser());
 //models
 const { User } = require('./models/user')
 const { Brand } = require('./models/brand')
+const { Wood } = require('./models/wood')
+const { Product } = require('./models/product')
 
 //Middlewares
 
 const { auth } = require('./middleware/auth')
+const { admin } = require('./middleware/admin')
 
+////=========================================
+
+//             PRODUCTS
+
+//===========================================
+
+
+// by Arrival
+// /articles?sortBy=createdAt&order=desc&limit=4
+
+// by sell
+// articles?sortBy=sold&order=desc&limit=4
+
+
+app.get('/api/product/articles',(req,res)=>{
+    let order = req.query.order ? req.query.order : 'asc';
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+    let limit = req.query.limit ? parseInt(req.query.limit) : 100;
+
+    Product.find().
+    populate('brand').
+    populate('wood').
+    sort([[sortBy,order]]).
+    limit(limit).
+    exec((err,articles)=>{
+        if(err) return res.status(400).send(err);
+        res.send(articles)
+    })
+})
+
+
+app.get('/api/product/articles_by_id',(req,res)=>{
+    let type = req.query.type;
+    let items = req.query.id;
+
+    if(type === "array"){
+        let ids = req.query.id.split(',');
+        items = [];
+        items = ids.map(item => {
+            return mongoose.Types.ObjectId(item)
+        })
+    }
+Product.find({ '_id':{$in:items}}).
+populate('brand').
+populate('wood').
+exec((err,docs) => {
+return res.status(200).send(docs)
+})
+})
+
+
+
+app.post('/api/product/article',auth,admin,(req,res)=>{
+    const product= new Product(req.body);
+    
+    product.save((err,doc)=>{
+        if(err) return res.json({sucess: false,err});
+        res.status(200).json({
+            success: true,
+            article: doc
+        })
+    })
+    })
+////=========================================
+
+//             WOODS
+
+//===========================================
+
+app.post('/api/product/wood', auth,admin,(req,res)=>{
+const wood= new Wood(req.body);
+
+wood.save((err,doc)=>{
+    if(err) return res.json({sucess: false,err});
+    res.status(200).json({
+        success: true,
+        wood: doc
+    })
+})
+})
+
+app.get('/api/product/woods', (req,res)=>{
+    Wood.find({},(err,woods)=>{
+        if(err) return res.status(400).send(err);
+        res.status(200).send(woods)
+    })
+})
+ 
 ////=========================================
 
 //             BRANDS
